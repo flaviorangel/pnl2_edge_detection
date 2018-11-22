cd24505b5d44e80076806c986497129185cb9e7f
flaviorangel <flm.rangel@gmail.com>
2018-11-22 18:57:41 -0200

    dilation and erosion are now the same function

--- morphology.py
+++ morphology.py
@@ -1,42 +1,16 @@
 import numpy as np
 
 
-def dilation(binary_image, structuring_element):
-    """Given a binary representation of an image, apply Hit to it.
+def dilation_or_erosion(binary_image, se, hit, erode_border=False):
+    """Given a binary representation of an image, apply Hit or Fit to its every pixel.
+    If Hit is applied, the resulting image is the original image dilated.
+    Otherwise, if Fit is applied, the resulting image is the original image eroded.
 
-    :param binary_image: a x*y matrix of 0s and 1s. 0 represents black; 1, white.
-    :param structuring_element: a n*n square of 0s and 1s. n must be an odd number.
-    :return: new image with Hit applied.
-    """
-    height = len(binary_image)
-    width = len(binary_image[0])
-    new_image = np.copy(binary_image)
-    for i in range(0, height):
-        for j in range(0, width):
-            for k in range(-1, 2):
-                if i + k >= height or i + k < 0:
-                    continue
-                hit = False
-                for a in range(-1, 2):
-                    if j + a >= width or j + a < 0:
-                        continue
-                    if binary_image[i + k][j + a] == 1:
-                        hit = True
-                        break
-                if hit:
-                    new_image[i][j] = 1
-                    break
-    return new_image
-
-
-def erosion(binary_image, se, erode_border=False):
-    """Given a binary representation of an image, apply Fit to it.
-    The structuring element is a 5x5 box.
-
-    :param binary_image: a x*y matrix of 0s and 1s. 0 represents black; 1, white.
+    :param binary_image: a x*y matrix of 0s and 1s. 0 represents black (background); 1, white (the objects).
     :param se: the structuring element. A n*n square of 0s and 1s, n must be an odd number.
-    :param erode_border: boolean. If True, will apply erosion when the part of the SE is in the border.
-    :return: new image with Fit applied.
+    :param hit: boolean. If True, Hit will be applied. If False, erosion will be applied.
+    :param erode_border: boolean. If True, will apply Fit when the part of the SE is in the border.
+    :return: new image with Hit or Fit applied to every pixel.
     """
     se_size = len(se)
     se_center = se_size // 2
@@ -47,28 +21,32 @@ def erosion(binary_image, se, erode_border=False):
         for j in range(0, width):
             se_i = -1
             se_j = -1
-            fit = False
+            do_hit_or_fit = False
             for p in range(-se_center, se_center + 1):
                 se_i += 1
                 if i + p >= height or i + p < 0:
-                    if erode_border and se[se_i][se_j]:
-                        fit = True
+                    if not hit and erode_border and se[se_i][se_j]:
+                        do_hit_or_fit = True
                         break
                     continue
                 for q in range(-se_center, se_center + 1):
                     se_j += 1
                     if j + q >= width or j + q < 0:
-                        if erode_border and se[se_i][se_j]:
-                            fit = True
+                        if not hit and erode_border and se[se_i][se_j]:
+                            do_hit_or_fit = True
                             break
                         continue
-                    if binary_image[i + p][j + q] == 0:
-                        fit = True
+                    if (hit and binary_image[i + p][j + q] == 1) or \
+                       (not hit and binary_image[i + p][j + q] == 0):
+                        do_hit_or_fit = True
                         break
-                if fit:
+                if do_hit_or_fit:
                     break
-            if fit:
-                new_image[i][j] = 0
+            if do_hit_or_fit:
+                if hit:
+                    new_image[i][j] = 1
+                else:
+                    new_image[i][j] = 0
     return new_image
 
 
@@ -92,7 +70,5 @@ if __name__ == "__main__":
     test_image[6][8] = 1
     se_3 = np.ones(shape=(3, 3))
     print(test_image)
-    # print(' ')
-    # print(dilation(test_image, se_3))
     print(' ')
-    print(erosion(test_image, se_3))
+    print(dilation_or_erosion(test_image, se_3, True))
